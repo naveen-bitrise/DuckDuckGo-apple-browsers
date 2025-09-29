ee5eb1bdefda3fdcb38ca9f1f388e9615f2292b8
Naveen Nazimudeen <naveen.nazimudeen@bitrise.io>
2025-09-28 21:15:40 -0400

    Creating the MockFeatureFlagger with just the   internalUserDecider parameter

--- iOS/DuckDuckGo/AppLifecycle/AppDependencyProvider.swift
+++ iOS/DuckDuckGo/AppLifecycle/AppDependencyProvider.swift
@@ -177,7 +177,8 @@ final class AppDependencyProvider: DependencyProvider {
         var featureFlagger: FeatureFlagger
         if [.unitTests, .integrationTests, .xcPreviews].contains(AppVersion.runType) || isTesting {
             // In testing mode, create MockFeatureFlagger with no enabled features to prevent network calls
-            let mockFeatureFlagger = MockFeatureFlagger(internalUserDecider: internalUserDecider, enabledFeatureFlags: [])
+            let mockFeatureFlagger = MockFeatureFlagger(internalUserDecider: internalUserDecider)
+            mockFeatureFlagger.enabledFeatureFlags = []
             self.contentScopeExperimentsManager = MockContentScopeExperimentManager()
             self.featureFlagger = mockFeatureFlagger
             featureFlagger = mockFeatureFlagger
